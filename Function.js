3a75ca39516d32acad104a4169aa3c31dbfa3f9d
bigveloper <bigveloper@gmail.com>
2021-09-17 01:28:46 +0900

    commit first Arrow Function

--- Function.js
+++ Function.js
@@ -58,29 +58,41 @@
 //
 //
 
-function getGrade(score) {
-    if (score === 100) {
-        return 'A+';
-    } else if (score >= 90) {
-        return 'A';
-    } else if (score === 89) {
-        return 'B+';
-    } else if (score >= 80) {
-        return 'B';
-    } else if (score === 79) {
-        return 'C+';
-    } else if (score >= 70) {
-        return 'C';
-    } else if (score === 69) {
-        return 'D+';
-    } else if (score >= 60) {
-        return 'D';
-    } else {
-        return 'F';
-    }
-}
+// function getGrade(score) {
+//     if (score === 100) {
+//         return 'A+';
+//     } else if (score >= 90) {
+//         return 'A';
+//     } else if (score === 89) {
+//         return 'B+';
+//     } else if (score >= 80) {
+//         return 'B';
+//     } else if (score === 79) {
+//         return 'C+';
+//     } else if (score >= 70) {
+//         return 'C';
+//     } else if (score === 69) {
+//         return 'D+';
+//     } else if (score >= 60) {
+//         return 'D';
+//     } else {
+//         return 'F';
+//     }
+// }
 
-const grade = getGrade(30);
-console.log(grade);
+// const grade = getGrade(30);
+// console.log(grade);
+// // 결과 : 30
 
-// 결과 : 30
+//
+//
+//
+
+// 화살표 함수 (Arrow Function)
+// 화살표 함수의 기본 모양 ()=>{}
+const add = (a, b) => {
+    return a + b;
+};
+const sum = add(1, 2);
+console.log(sum);
+// 결과 : 3
