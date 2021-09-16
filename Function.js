ff335cb656e2712a8d05f515a5714e87ae6d360f
bigveloper <bigveloper@gmail.com>
2021-09-17 00:23:18 +0900

    commit first function

---
+++ Function.js
@@ -0,0 +1,13 @@
+// const a = 1;
+// const b = 2;
+// const sum = a + b;
+// console.log(sum);
+// // 결과 : 3
+
+function add(a, b) {
+    return a + b;
+}
+
+const sum = add(1, 2);
+console.log(sum);
+// 결과 : 3
