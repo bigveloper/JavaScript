3f6eec021745c36bb8b902b90e1a9570ec2b6d00
bigveloper <bigveloper@gmail.com>
2021-09-16 22:34:12 +0900

    commit first ternaryOperator

---
+++ ternaryOperator.js
@@ -0,0 +1,15 @@
+// 삼항 연산자
+
+// condition ? true : false
+// aespa ? nextLevel : rollin
+
+const array = [1, 2];
+let text = '';
+
+if (array.length === 0) {
+    text = '배열이 비어있습니다.';
+} else {
+    text = '배열이 비어있지 않습니다.';
+}
+
+console.log(text);
