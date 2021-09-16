880daa8f6063dc70df4c0f37c001ab5eb090959b
bigveloper <bigveloper@gmail.com>
2021-09-16 22:48:06 +0900

    commit fourth ternaryOperator

--- ternaryOperator.js
+++ ternaryOperator.js
@@ -20,12 +20,25 @@
 
 //
 
-// 삼항 연산자를 사용한 코드
+// // 삼항 연산자를 사용한 코드
 
-const array = [1, 2];
-let text = array.length === 0 ? '배열이 비어있습니다.' : '배열이 비어있지 않습니다.';
+// const array = [1, 2];
+// let text = array.length === 0 ? '배열이 비어있습니다.' : '배열이 비어있지 않습니다.';
 
-console.log(text);
-// 결과 : '배열이 비어있지 않습니다.'
+// console.log(text);
+// // 결과 : '배열이 비어있지 않습니다.'
 
-// const array = []; 라면, 배열이 비어있습니다. 출력
+// // const array = []; 라면, 배열이 비어있습니다. 출력
+
+//
+
+// 중첩해서 사용하는 삼항 연산자
+
+const condition1 = false;
+const condition2 = false;
+
+const value = condition1 ? '와우!' : condition2 ? 'blabla' : 'foo';
+
+console.log(value);
+
+// 결과 : foo
