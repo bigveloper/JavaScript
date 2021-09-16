3212d1b5d2fac960fc2e3e3ea14bb66c7abf378d
bigveloper <bigveloper@gmail.com>
2021-09-17 00:42:50 +0900

    commit third function Tamplate Literal

--- Function.js
+++ Function.js
@@ -14,13 +14,21 @@
 
 //
 
-function hello(name) {
-    console.log('Hello, ' + name + '!');
-}
-hello('WhyJ');
-// 결과 : Hello, WhyJ!
+// ES6 prevVersion
+// function hello(name) {
+//     console.log('Hello, ' + name + '!');
+// }
+// hello('WhyJ');
+// // 결과 : Hello, WhyJ!
 
 // function 이라는 키워드로 선언한다.
 // function 에는 이름이 필요하다.
 // 이름 옆에는 파라미터(함수로 받을 값) 을 ()둥근괄호 안에 선언한다.
 // {} 중괄호 안에서는 실행구문 을 선언한다.
+
+// ES6 Version
+function hello(name) {
+    console.log(`Hello ${name}!`);
+}
+hello('WhyJ');
+// 결과 : Hello WhyJ!
