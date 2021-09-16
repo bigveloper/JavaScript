6212fc8ab0b113a20bfef0550fa9f713f8e3088e
bigveloper <bigveloper@gmail.com>
2021-09-16 22:36:20 +0900

    commit second ternaryOperator

--- ternaryOperator.js
+++ ternaryOperator.js
@@ -3,13 +3,19 @@
 // condition ? true : false
 // aespa ? nextLevel : rollin
 
-const array = [1, 2];
-let text = '';
+// // 삼항 연산자를 사용하지 않은 코드
+// const array = [1, 2];
+// let text = '';
 
-if (array.length === 0) {
-    text = '배열이 비어있습니다.';
-} else {
-    text = '배열이 비어있지 않습니다.';
-}
+// if (array.length === 0) {
+//     text = '배열이 비어있습니다.';
+// } else {
+//     text = '배열이 비어있지 않습니다.';
+// }
 
-console.log(text);
+// console.log(text);
+
+// // 결과 : 배열이 비어있지 않습니다.
+// // const array = []; 라면, 배열이 비어있습니다. 출력
+
+// 삼항 연산자를 사용한 코드
